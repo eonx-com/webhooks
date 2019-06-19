0d038287cc4142e489f6d7e1e5d457cc3d9886f3
Rashmit Singh <rashmit@loyaltycorp.com.au>
2019-06-19 14:37:21 +1000

    Webhook request repository updated query to use provided date

--- src/Bridge/Doctrine/Repositories/WebhookRequestRepository.php
+++ src/Bridge/Doctrine/Repositories/WebhookRequestRepository.php
@@ -3,46 +3,46 @@ declare(strict_types=1);
 
 namespace EoneoPay\Webhooks\Bridge\Doctrine\Repositories;
 
+use DateTime;
 use Doctrine\ORM\Query;
 use EoneoPay\Externals\ORM\Repository;
+use EoneoPay\Webhooks\Bridge\Doctrine\Entities\WebhookRequest;
 use EoneoPay\Webhooks\Bridge\Doctrine\Entities\WebhookResponse;
 
 class WebhookRequestRepository extends Repository
 {
     /**
-     * Get list of webhook requests that have passed
+     * Get list of webhook requests that have failed since provided date time
+     *
+     * @param \DateTime $since Datetime since there have been failed webhook requests
      *
      * @return \Doctrine\ORM\Query
      */
-    public function getPassedRequests(): Query
+    public function getFailedRequests(DateTime $since): Query
     {
         $buildResponse = $this->entityManager
             ->createQueryBuilder()
             ->from(WebhookResponse::class, 's');
 
-        $buildResponse->select('DISTINCT(s.request)')
-            ->where($buildResponse->expr()->eq('s.statusCode', ':statusCode'));
-
-        $buildResponse->setParameters([
-            'statusCode' => 200
-        ]);
-
-        $requestPassed = $buildResponse->getQuery()->getResult();
-
-        $buildRequest = $this->createQueryBuilder('q');
+        $buildRequest = $this->entityManager
+            ->createQueryBuilder()
+            ->from(WebhookRequest::class, 'q');
 
         $buildRequest
-            ->where(
-                $buildRequest->expr()->notIn('q.requestId', ':requestIdNotIn')
-            );
+            ->select('q.requestId')
+            ->where($buildRequest->expr()->notIn('q.requestId', ':requestIdNotIn'))
+            ->andWhere($buildRequest->expr()->gte('q.createdAt', ':createdAt'));
 
         $buildRequest->setParameters([
-            'requestIdNotIn' => $requestPassed
+            'requestIdNotIn' => $buildResponse->select('DISTINCT(s.request)')
+                ->where($buildResponse->expr()->eq('s.statusCode', ':statusCode'))
+                ->setParameters([
+                    'statusCode' => 200
+                ])->getDQL(),
+            'createdAt' => $since->format('Y:m:d H:i:s')
         ]);
 
-        $query = $buildRequest->getQuery();
-        $requests = $query->getResult();
+        return $buildRequest->getQuery();
 
-        return $query;
     }
 }
